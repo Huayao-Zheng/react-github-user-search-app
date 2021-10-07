5b412aca01e627d74ad63b415fafb9e3f5db7953
lonelybuddy <henryzheng92@gmail.com>
2021-10-07 01:14:18 -0500

    added style for header, light/dark theme toggles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import './App.css';
 import Header from './components/Header/Header';
 import Searchbar from './components/Searchbar/Searchbar';
 import User from './components/User/User';
 
 function App() {
   return (
-    <div>
+    <>
       <Header></Header>
       <Searchbar></Searchbar>
       <User></User>
-    </div>
+    </>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Header.css';
+import moon from '../../assets/icon-moon.svg';
+import sun from '../../assets/icon-sun.svg';
 
 const Header = () => {
-  return <div>Header</div>;
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const onThemeClick = () => {
+    const modeToggle = document.getElementById('root');
+
+    setIsDarkMode((boolean) => !boolean);
+    modeToggle.classList.toggle('dark-theme');
+  };
+
+  return (
+    <header>
+      <div className="header_logo-text">devfiner</div>
+      <div className="header_light-dark-mode" onClick={onThemeClick}>
+        {isDarkMode ? (
+          <>
+            <span>LIGHT</span>
+            <img src={sun} alt="sun" className="sun" />
+          </>
+        ) : (
+          <>
+            <span>DARK</span>
+            <img src={moon} alt="moon" className="moon" />
+          </>
+        )}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
