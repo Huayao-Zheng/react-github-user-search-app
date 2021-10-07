8246ab68f5020ed7595e0c5274d99bd0bb321427
lonelybuddy <henryzheng92@gmail.com>
2021-10-07 16:15:42 -0500

    created and styled Searchbar component

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Searchbar.css';
+
+const searchClass = (
+  <svg
+    viewBox="0 0 25 25"
+    className="search-magnifier"
+    height="20px"
+    width="20px"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M10.609 0c5.85 0 10.608 4.746 10.608 10.58 0 2.609-.952 5-2.527 6.847l5.112 5.087a.87.87 0 01-1.227 1.233l-5.118-5.093a10.58 10.58 0 01-6.848 2.505C4.759 21.16 0 16.413 0 10.58 0 4.747 4.76 0 10.609 0zm0 1.74c-4.891 0-8.87 3.965-8.87 8.84 0 4.874 3.979 8.84 8.87 8.84a8.855 8.855 0 006.213-2.537l.04-.047a.881.881 0 01.058-.053 8.786 8.786 0 002.558-6.203c0-4.875-3.979-8.84-8.87-8.84z"
+      fill="#0079ff"
+    />
+  </svg>
+);
 
 const Searchbar = () => {
-  return <div>searchbar</div>;
+  const [input, setInput] = useState('');
+
+  const onInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const onSearch = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div id="form-container">
+      <form onSubmit={onSearch}>
+        {searchClass}
+        <input
+          className="input"
+          value={input}
+          onChange={onInputChange}
+          placeholder="Search GitHub username…"
+        />
+        <input type="submit" value="Search" className="search-btn" />
+      </form>
+    </div>
+  );
 };
 
 export default Searchbar;
