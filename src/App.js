e094fb75ccad11ba8a500c7994bbafb1fe37ce27
lonelybuddy <henryzheng92@gmail.com>
2021-10-06 20:09:52 -0500

    created basic components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import Header from './components/Header/Header';
+import Searchbar from './components/Searchbar/Searchbar';
+import User from './components/User/User';
 
 function App() {
-  return <div className="App">hellsadfoasds</div>;
+  return (
+    <div>
+      <Header></Header>
+      <Searchbar></Searchbar>
+      <User></User>
+    </div>
+  );
 }
 
 export default App;
