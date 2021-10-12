126171c49b6fef367df4c133a8aa10f79bbb29db
lonelybuddy <henryzheng92@gmail.com>
2021-10-11 21:39:58 -0500

    finished naming for user header

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import './User.css';
 
 const User = () => {
-  return <div>user</div>;
+  return (
+    <div className="user-pofile-container">
+      <div className="user">
+        <div className="user__avatar-container">
+          <img className="user__avatar" src="" alt="avatar" />
+        </div>
+        <div className="user__names-container">
+          <div className="user__name">The Octocat</div>
+          <div className="user__login"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default User;
