6d047dcc92b7634e3df7ff1cf514fc7c42a710c8
lonelybuddy <henryzheng92@gmail.com>
2021-10-11 22:32:23 -0500

    finished user component. ready to style next

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,16 +1,52 @@
 import React from 'react';
 import './User.css';
 
-const User = () => {
+const User = ({ userProfile }) => {
+  if (!userProfile) {
+    return <></>;
+  }
+
+  const {
+    name,
+    login,
+    created_at,
+    bio,
+    public_repos,
+    followers,
+    following,
+    avatar_url,
+  } = userProfile;
+
   return (
-    <div className="user-pofile-container">
-      <div className="user">
+    <div className="user">
+      <div className="user_header-container">
         <div className="user__avatar-container">
-          <img className="user__avatar" src="" alt="avatar" />
+          <img className="user__avatar" src={avatar_url} alt="avatar" />
         </div>
         <div className="user__names-container">
-          <div className="user__name">The Octocat</div>
-          <div className="user__login"></div>
+          <div className="user__name">{name}</div>
+          <div className="user__login">{login}</div>
+          <div className="user_joined-day">{created_at}</div>
+        </div>
+      </div>
+
+      <div className="user__bio">{bio ? bio : 'Oh! Such empty...'}</div>
+
+      <div className="user_repos-followers-following-contianer">
+        <div className="user_repos">
+          Repos
+          <br />
+          <strong>{public_repos}</strong>
+        </div>
+        <div className="user_followers">
+          Followers
+          <br />
+          <strong>{followers}</strong>
+        </div>
+        <div className="user_following">
+          Following
+          <br />
+          <strong>{following}</strong>
         </div>
       </div>
     </div>
